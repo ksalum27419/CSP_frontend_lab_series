e23402b05853297ef0064233039be8c9aadd6555
ksalum27419 <ksalum27419@columbushs.com>
2025-10-29 11:53:44 -0400

    Finished 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -4,18 +4,35 @@
    =========================================================== */
 
 // TODO 1: Print your full name with console.log()
-
+console.log("Kris Salum")
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
-
-
+let favoriteFood = "steak"
+console.log(favoriteFood)
+favoriteFood = "pizza"
+console.log(favoriteFood)
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
 
+let mystery = "Hi"
+console.log(typeof mystery)
+mystery= 34
+console.log(typeof mystery)
 
 // TODO 4: Declare const schoolName, try to reassign
+const schoolName = "Christopher Columbus"
+//console.log(schoolName)
+
 
 
 // TODO 5: Create three valid camelCase variables and print them
+let favorite= "pizza"
+let favoriteHoliday = "christmas"
+let favoriteSchool = "Christopher Columbus"
+
+print(favoriteFood)
+print(favoriteHoliday)
+print(favoriteSchool)
+
 
 
 // 🚀 Stretch Goals (optional)
@@ -25,6 +42,9 @@
 /* ===========================================================
    🎟 EXIT TICKET
    1. What new concept or skill did you learn in this lab?
+   New skill that I learn in thsi lab is let and const. I am able to now use them more effeciently because I know the purposes they both provide. 
    2. What challenges did you encounter, and how did you solve them?
+   Challenges I encouter was I would put what the variable was assigned in the console.log instead of the variable itself. I solve by chekcing and adding the variable into teh console.log()
    3. Where else could this programming concept be useful?
+   This progarmming concept be useful in math because this could help you with analytcial thinking. This could also help you in physics because it one mistake could cost you the asnswer but cafeully checking could help fix it.
    =========================================================== */
